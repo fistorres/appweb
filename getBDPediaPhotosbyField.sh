f0ba4c3c011d18db52e4ba6536f4d8edf23cd9c5
fistorres <45104156+fistorres@users.noreply.github.com>
2020-03-25 16:45:10 +0000

    Update getBDPediaPhotosbyField.sh

--- getBDPediaPhotosbyField.sh
+++ getBDPediaPhotosbyField.sh
@@ -5,24 +5,18 @@
 inc=0
 name=""
 
-# On my computer I used the command bellow, however I don't think that this server version of xmllint supports it:
+# On my computer I used the command bellow with no extra need for grep or sed, however I don't think that this server version of xmllint supports it:
 # xmllint --xpath '//*[local-name()="binding"][@name="'name'" or @name="'image'"]//text()' $1.xml
-# In this server, results appear with no newline between them and so are hard to parse.
-# also Im not sure but I think that xpath reads the text as utf. with grep things like "Alzheimer's diseases" end poorly written. Im not able to convert it.
 
-
-grep 'name="image"\|name="name"' $1.xml | sed -e "s/<[^>]*>//g" -e "s/^ *//" -e "s/ *$//" | while read -r line; do
+grep 'name="image"\|name="name"' $1.xml | sed -e "s/<[^>]*>//g" -e "s/^ *//" -e "s/ *$//"-e "s/&#39;/'/" -e "/^[[:space:]]*$/d" | while read -r line; do
         if [ $((inc%2)) -eq 0 ];
-
         then
                 name=${line// /}
                 echo $name >> diseases.txt
                 echo "Disease: $name"
         else
-                echo "$line" > "$name""BDpediaPhotos.txt"
+                echo "$line" > "$name""DBpediaPhotos.txt"
         fi
         ((inc++))
 
 done
-
-                                                                                                
